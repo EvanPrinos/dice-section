30754980c7cb9134287d04b762155bceaeeedbcf
Tester <92867599+EvanPrinos@users.noreply.github.com>
2025-11-26 18:04:02 -0500

    Bada bing bada boom

--- script.js
+++ script.js
@@ -6,14 +6,15 @@ function toggleNav() {
     // Close the sidenav
     popoutsidenav.style.width = "0vw";
     popoutsidenav.style.border = "0px, solid, #22B5F6";
-    topleftbutton.style.color = "#22B5F6";
+    //topleftbutton.style.color = "#22B5F6";
+    topleftbutton.classList.remove("active");
 
   } else {
     // Open the sidenav
     popoutsidenav.style.width = "35vw";
     popoutsidenav.style.border = "1px, solid, #22B5F6";
-    topleftbutton.style.color = "#D0BD45";
-
+    //topleftbutton.style.color = "#D0BD45";
+    topleftbutton.classList.add("active");
 
   }
 }
