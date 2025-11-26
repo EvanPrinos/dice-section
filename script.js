fa47587e3082908ec930fa6cdf076b16b1b3bab8
Tester <92867599+EvanPrinos@users.noreply.github.com>
2025-11-26 12:59:00 -0500

    Tentative Sidebar changes

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ function toggleNav() {
   if (popoutsidenav.style.width === "35vw") {
     // Close the sidenav
     popoutsidenav.style.width = "0vw";
+    popoutsidenav.style.border = "0px, solid, #22B5F6";
+
   } else {
     // Open the sidenav
     popoutsidenav.style.width = "35vw";
+    popoutsidenav.style.border = "1px, solid, #22B5F6";
+
   }
 }
