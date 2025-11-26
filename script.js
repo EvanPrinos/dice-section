20c79e58dbbada515894997b10a397a6f78c2b94
Tester <92867599+EvanPrinos@users.noreply.github.com>
2025-11-26 15:22:14 -0500

    Fonts work

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 function toggleNav() {
   const popoutsidenav = document.getElementById("popoutsidenav");
+  const topleftbutton = document.getElementsByClassName("topleftbutton");
 
   if (popoutsidenav.style.width === "35vw") {
     // Close the sidenav
     popoutsidenav.style.width = "0vw";
     popoutsidenav.style.border = "0px, solid, #22B5F6";
+    topleftbutton.style.textcolor = "#22B5F6";
 
   } else {
     // Open the sidenav
     popoutsidenav.style.width = "35vw";
     popoutsidenav.style.border = "1px, solid, #22B5F6";
+    topleftbutton.style.textcolor = "#D0BD45";
+
 
   }
 }
